f4bcc66d69e38acd0f41c2bb6fc2ca46755ae6a2
Roberto Ríos <roberto@deveahub.com>
2024-01-24 13:54:09 +0100

    feat: enhance astro build with sitemaps and compress assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
-import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
+import { defineConfig } from 'astro/config';
 import million from 'million/compiler';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+const NAME_WEBSITE = 'https://app.rrios.dev';
+
 export default defineConfig({
+  site: NAME_WEBSITE,
   integrations: [
     million.vite({ mode: 'react', server: true, auto: true }),
     react(),
     tailwind(),
+    sitemap(),
+    compress(),
   ],
 });
